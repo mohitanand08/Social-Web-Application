e08a84281b8b877cb2847a06e731dcbf5eaa91c7
Mohit Anand <mohitanand0810@gmail.com>
2019-12-27 14:20:17 +0530

    added routes and controllers

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.listen(port, function(err){
-    if(err){
-        console.log(`Error in runnning the server: ${err}`);
-        return;
-    }
-    console.log(`Server is running in port : ${port}`);
-    
+// use express router
+app.use('/', require('./routers'));
+
+app.listen(port, function(err) {
+	if (err) {
+		console.log(`Error in runnning the server: ${err}`);
+		return;
+	}
+	console.log(`Server is running in port : ${port}`);
 });
---
+++ routers/index.js
@@ -0,0 +1,8 @@
+// It fetches the existing express does not creates a new express
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controllers/home_Controller');
+// Router is accessing the home Controller
+router.get('/', homeController.home);
+module.exports = router;
