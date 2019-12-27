4b1bb768cfebe7f1510d6e5d2015cd273c167066
Mohit Anand <mohitanand0810@gmail.com>
2019-12-27 14:51:38 +0530

    another router and controller is added and also view engine is setup

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 // use express router
 app.use('/', require('./routers'));
 
+// setting up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.listen(port, function(err) {
 	if (err) {
 		console.log(`Error in runnning the server: ${err}`);
--- routers/index.js
+++ routers/index.js
@@ -5,4 +5,5 @@ const router = express.Router();
 const homeController = require('../controllers/home_Controller');
 // Router is accessing the home Controller
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
 module.exports = router;
